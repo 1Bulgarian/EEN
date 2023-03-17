c9ce6f7a338a77d0ec0d667b34690bb0a20bbea7
1Bulgarian <moni300403@abv.bg>
2023-03-18 01:43:03 +0200

    Beginning
    #1

---
+++ EEN/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EEN.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
